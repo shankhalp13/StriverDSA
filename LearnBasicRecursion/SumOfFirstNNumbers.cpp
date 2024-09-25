88a862ada6eb922cc670982ff5462cd13b6d7d63
shankhalp13 <shankhalppradhan2003@gmail.com>
2024-09-25 10:29:29 +0530

    Sum of First N numbers

--- LearnBasicRecursion/SumOfFirstNNumbers.cpp
+++ LearnBasicRecursion/SumOfFirstNNumbers.cpp
@@ -16,17 +16,38 @@ using namespace std;
 //     cout<<"Sum is "<<sum <<endl;
 // }
 
+// Another Method I though which I learned using Prolog
+//  int sum(int n){
+//      if(n==1) return 1;
+//      return (sum(n-1)+n);
+//  }
 
-//Another Method I though which I learned using Prolog
-int sum(int n){
-    if(n==1) return 1;
-    return (sum(n-1)+n);
+// int main(){
+//     int n,total;
+//     cout<<"Enter the number n:";
+//     cin >> n;
+//     total = sum(n);
+//     cout<<"Sum is "<<total <<endl;
+// }
+
+// Parameterized method
+void add(int i, int sum)
+{
+    if (i < 1)
+    {
+        cout << sum << endl;
+        return;
+    }
+
+    add(i - 1, sum + i);
+    return;
 }
 
-int main(){
-    int n,total;
-    cout<<"Enter the number n:";
+int main()
+{
+    int n;
+    cout << "Enter the number n:";
     cin >> n;
-    total = sum(n);
-    cout<<"Sum is "<<total <<endl;
+    add(n, 0);
+    return 0;
 }
